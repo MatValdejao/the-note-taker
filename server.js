99700a466e6ac13ee0305ed91fadcbc26fbe5351
MatValdejao <matvaldejao123@gmail.com>
2022-08-17 12:18:31 -0400

    route to default index.html ready

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const PORT = 3001 || process.env.PORT
 
@@ -7,6 +8,15 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
+})
+
+app.get("/notes", (req, res) => {
+    let notes = res
+
+    console.log(notes)
+})
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
