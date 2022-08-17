11dbb16c80ed8406f1cbf0dd99e76138c3a59309
MatValdejao <matvaldejao123@gmail.com>
2022-08-17 12:10:22 -0400

    added first middleware

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = 3001 || process.env.PORT
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 })
