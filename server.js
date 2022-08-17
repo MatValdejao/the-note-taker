516a74261b04083c5378eaef6fd2e302635aa4f8
MatValdejao <matvaldejao123@gmail.com>
2022-08-17 15:02:19 -0400

    delete works upon page reload, now update as soon as delete clicked

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs")
 const path = require("path");
 const { notes } = require("./Develop/db/db.json");
 const createNewNote = require("./lib/notes");
@@ -10,6 +11,7 @@ const app = express();
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// sets static files path
 app.use(express.static("Develop/public"))
 
 // html route to display index onto page
@@ -32,12 +34,30 @@ app.get("/api/notes", (req, res) => {
 // will post notes once added by user
 app.post("/api/notes", (req, res) => {
 	// sets an id to individual notes to keep track
-	req.body.id = notes.length.toString();
+    req.body.id = notes.length.toString();
 
 	// creates note
 	const note = createNewNote(req.body, notes);
-	res.json(note);
+    res.json(note);
 });
+
+// delete notes
+app.delete(`/api/notes/:id`, (req, res) => {   
+    // reconcile between length and indexing
+    const id = req.params.id + 1
+
+    // ensures note exists
+    if (req.body) {
+        // goes through notes array to check which ID matches
+        const noteIndex = notes.findIndex(index => index.id === id)
+        notes.splice(noteIndex, 1)
+
+        fs.writeFileSync(path.join(__dirname, "./Develop/db/db.json"), JSON.stringify({notes: notes}), null, 2)
+    } else {
+        console.log("Note not found.")
+        return
+    }   
+})
 
 // deals with incorrect requests
 app.get("*", (req, res) => {
