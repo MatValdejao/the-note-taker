39ae1ea7b87b81fbba12c3bf370cab0606a1a13e
MatValdejao <matvaldejao123@gmail.com>
2022-08-17 22:54:15 -0400

    changed PORT orde

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const apiRoutes = require("./routes/apiRoutes")
 const htmlRoutes = require("./routes/htmlRoutes")
 
-const PORT = 3001 || process.env.PORT;
+const PORT = Process.env.PORT || 3001;
 
 const app = express();
 
